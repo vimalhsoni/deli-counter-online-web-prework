2699b0782320f957a34de37bd4ce2e3103dec53f
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-17 18:19:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,10 @@
 def line(array)
-  if array == 0 
+  if array == 0
     puts "The line is currently empty."
-  else 
+  else
     string = "The line is currently:"
     array.each_with_index do |person, index|
       string += "#{index + 1}. #{person}"
     end
     puts string
   end
-  
