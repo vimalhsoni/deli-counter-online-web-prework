89727ab930a560a46ae6e66ffd270b829338c92d
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-17 18:28:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,14 @@ def take_a_number(array,person)
     puts "Welcome, #{person}. You are number #{index + 1} in line."
 end
 end
+
+def now_serving(array)
+  if array.size > 0
+  currently_serving = array.first
+  puts "Currently serving #{currently_serving}."
+elsif array.size == 0 
+  puts "There is nobody waiting to be served!"
+end
+end
+
+  
